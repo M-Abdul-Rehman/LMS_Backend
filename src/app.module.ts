097f4774f6beef034aefe24f6535fc360c13dc3d
M-Abdul-Rehman <aarehman945@gmail.com>
2025-07-08 17:23:33 +0500

    Changed Local Database to Railway Databse

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,11 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'LMS',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'LMS',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
